9c17ce0d3fcfb7436eb6affc1cdaee7b37e4ac08
Anna Yakovleva <89506212679@mail.ru>
2020-11-11 17:26:15 +0300

    all

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('URL', 'http://localhost/18_task'); // URL текущей страницы
+define('UPLOAD_MAX_SIZE', 1000000); // 1mb
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
+define('UPLOAD_DIR', 'gallery');
+define('COMMENT_DIR', 'comments');
